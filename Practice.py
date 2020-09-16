869e70800cbf9d8b0899d055a197cbdc16aa5b30
Bayne <68715770+bvardy@users.noreply.github.com>
2020-09-16 16:18:39 -0700

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+    def __str__(self):
+        return self.data
+
+class LinkedLists:
+    def __init__(self, nodes=None):
+        self.head = None
+        if nodes is not None:
+            node = Node(nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(elem)
+                node = node.next
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node # maybe add .data
+            node = node.next
+
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+llst = LinkedLists(["1", "2", "5", "10"])
+llst2 = LinkedLists(["1", "3", "7", "9"])
+
+def mergeLinkedLists(arr1, arr2):
+    arr1_list = []
+    for i in arr1:
+        arr1_list.append(i.data)
+
+    arr2_list = []
+    for j in arr2:
+        arr2_list.append(j.data)
+
+    arr3_list = []
+    i = 0
+    j = 0
+    while i < len(arr1_list) and j < len(arr2_list):
+        if int(arr1_list[i]) < int(arr2_list[j]):
+            arr3_list.append(arr1_list[i])
+            i += 1
+        else:
+            arr3_list.append(arr2_list[j])
+            j += 1
+
+    while i < len(arr1_list):
+        arr3_list.append(arr1_list[i])
+        i += 1
+
+    while j < len(arr2_list):
+        arr3_list.append(arr2_list[j])
+        j += 1
+
+    new_linkedlist = LinkedLists(arr3_list)
+    return new_linkedlist
+
+print(mergeLinkedLists(llst, llst2))
